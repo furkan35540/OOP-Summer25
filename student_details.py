6f452dc37e8358bf91408f124785e2629eb8a7e6
ahmetardabalkaya <ardabalkaya9@gmail.com>
2025-03-13 14:39:52 +0100

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,17 @@
+student1 = {
+    'first_name': 'Ahmet',
+    'second_name': 'Arda',
+    'last_name': 'Balkaya',
+    'nationality': 'Turkish',
+    'index_number': 35535,
+    'starting_date' : '10/10/2024'
+    'courses' : ['english', 'maths']
+}
+
+print(student1['first_name'])
+print(student1['second_name'])
+print(student1['last_name'])
+print(student1['nationality'])
+print(student1['index_number'])
+print(student1['starting_date'])
+print(student1['courses'])
