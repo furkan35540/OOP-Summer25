30f752acb5e91016187fcf528000ae440c635588
ahmetardabalkaya <ardabalkaya9@gmail.com>
2025-03-13 14:37:32 +0100

    Update student_details_structure.py

--- student_details_structure.py
+++ student_details_structure.py
@@ -3,7 +3,9 @@ student1 = {
     'second_name': 'Arda',
     'last_name': 'Balkaya',
     'nationality': 'Turkish',
-    'index_number': 35535
+    'index_number': 35535,
+    'starting_date' : '10/10/2024'
+    'courses' : ['english', 'maths']
 }
 
 print(student1['first_name'])
@@ -11,3 +13,5 @@ print(student1['second_name'])
 print(student1['last_name'])
 print(student1['nationality'])
 print(student1['index_number'])
+print(student1['starting_date'])
+print(student1['courses'])
