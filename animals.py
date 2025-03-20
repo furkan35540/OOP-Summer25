85d6367b1e97f414954502259c99d5d60e69dd9b
ahmetardabalkaya <ardabalkaya9@gmail.com>
2025-03-20 14:11:03 +0100

    Create animals.py

---
+++ animals.py
@@ -0,0 +1,32 @@
+animals = [
+    {
+        "name": "Cat",
+        "group": "Mammals",
+        "number_of_legs": 4,
+        "skills": ["jumping", "climbing", "hunting"]
+    },
+    {
+        "name": "Dog",
+        "group": "Mammals",
+        "number_of_legs": 4,
+        "skills": ["running", "hunting", "smelling"]
+    },
+    {
+        "name": "Frog",
+        "group": "Amphibians",
+        "number_of_legs": 4,
+        "skills": ["jumping", "swimming", "croaking"]
+    },
+    {
+        "name": "Shark",
+        "group": "Fish",
+        "number_of_legs": 0,
+        "skills": ["swimming", "hunting", "detecting vibrations"]
+    },
+    {
+        "name": "Snake",
+        "group": "Reptiles",
+        "number_of_legs": 0,
+        "skills": ["slithering", "camouflaging", "hunting"]
+    }
+]
