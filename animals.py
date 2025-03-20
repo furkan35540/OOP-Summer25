3a0ed6ea0e8d62d47be284dad3319a5f94dfb306
ahmetardabalkaya <ardabalkaya9@gmail.com>
2025-03-20 14:12:55 +0100

    Update animals.py

--- animals.py
+++ animals.py
@@ -1,32 +1,20 @@
-animals = [
-    {
-        "name": "Cat",
+animals = [ {  "name": "Cat",
         "group": "Mammals",
         "number_of_legs": 4,
-        "skills": ["jumping", "climbing", "hunting"]
-    },
-    {
-        "name": "Dog",
+        "skills": ["jumping", "climbing", "hunting"] },
+    {   "name": "Dog",
         "group": "Mammals",
         "number_of_legs": 4,
-        "skills": ["running", "hunting", "smelling"]
-    },
-    {
-        "name": "Frog",
+        "skills": ["running", "hunting", "smelling"] },
+    {   "name": "Frog",
         "group": "Amphibians",
         "number_of_legs": 4,
-        "skills": ["jumping", "swimming", "croaking"]
-    },
-    {
-        "name": "Shark",
+        "skills": ["jumping", "swimming", "croaking"] },
+    {   "name": "Shark",
         "group": "Fish",
         "number_of_legs": 0,
-        "skills": ["swimming", "hunting", "detecting vibrations"]
-    },
-    {
-        "name": "Snake",
+        "skills": ["swimming", "hunting", "detecting vibrations"] },
+    {   "name": "Snake",
         "group": "Reptiles",
         "number_of_legs": 0,
-        "skills": ["slithering", "camouflaging", "hunting"]
-    }
-]
+        "skills": ["slithering", "camouflaging", "hunting"] }]
