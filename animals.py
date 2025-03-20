a9a01c8efcc1a0ca9b1225dc4d954ea900dd6887
furkan35540 <furkanpilge23@gmail.com>
2025-03-20 13:27:43 +0000

    Animals.py

---
+++ animals.py
@@ -0,0 +1,37 @@
+
+animals = [
+    {
+        'name': 'Cat',
+        'group': 'Mammals',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'climbing', 'hunting']
+    },
+    {
+        'name': 'Eagle',
+        'group': 'Birds',
+        'number_of_legs': 2,
+        'skills': ['flying', 'hunting', 'sharp vision']
+    },
+    {
+        'name': 'Frog',
+        'group': 'Amphibians',
+        'number_of_legs': 4,
+        'skills': ['jumping', 'swimming', 'croaking']
+    },
+    {
+        'name': 'Shark',
+        'group': 'Fish',
+        'number_of_legs': 0,
+        'skills': ['swimming', 'hunting', 'detecting movement']
+    },
+    {
+        'name': 'Snake',
+        'group': 'Reptiles',
+        'number_of_legs': 0,
+        'skills': ['slithering', 'camouflage', 'venomous bite']
+    }
+]
+
+for animal in animals:
+    print(animal)
+    
